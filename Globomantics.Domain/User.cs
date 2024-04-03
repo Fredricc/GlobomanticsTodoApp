25a4a8fe589720ec17fd8c1114c18bb0d54d5e8a
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-04-03 19:00:29 +0300

    Add project files.

---
+++ Globomantics.Domain/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Globomantics.Domain
+{
+    public record  User(string Name)
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        //public string Name { get; init; }
+    }
+}
