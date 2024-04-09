137366773b74cd4f539e6e45e98d5e7209f09e67
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-04-09 21:43:41 +0300

    Creating and Injecting Additional Repositories

--- Globomantics.Infrastructure/Data/Repositories/IRepository.cs
+++ Globomantics.Infrastructure/Data/Repositories/IRepository.cs
@@ -1,11 +1,10 @@
-﻿namespace Globomantics.Infrastructure.Data.Repositories
+﻿namespace Globomantics.Infrastructure.Data.Repositories;
+
+public interface IRepository<T>
 {
-    public interface IRepository<T>
-    {
-        Task<T> GetAsync(Guid id);
-        Task<T> FindByAsync(string value);
-        Task<IEnumerable<T>> AllAsync();
-        Task AddAsync(T item);
-        Task SaveChangesAsync();
-    }
-}
+    Task<T> GetAsync(Guid id);
+    Task<T> FindByAsync(string value);
+    Task<IEnumerable<T>> AllAsync();
+    Task AddAsync(T item);
+    Task SaveChangesAsync();
+} 
--- Globomantics.Infrastructure/Data/Repositories/TodoInMemoryRepository.cs
+++ Globomantics.Infrastructure/Data/Repositories/TodoInMemoryRepository.cs
@@ -1,42 +1,41 @@
 ﻿using Globomantics.Domain;
 using System.Collections.Concurrent;
 
-namespace Globomantics.Infrastructure.Data.Repositories
+namespace Globomantics.Infrastructure.Data.Repositories;
+
+public class TodoInMemoryRepository<T> : IRepository<T>
+    where T : Todo
 {
-    public class TodoInMemoryRepository<T> : IRepository<T>
-        where T : Todo
+    private ConcurrentDictionary<Guid, T> Items { get; } = new();
+
+    public Task AddAsync(T item)
     {
-        private ConcurrentDictionary<Guid, T> Items {  get; } = new();
-        public Task AddAsync(T item)
-        {
-            Items.TryAdd(item.Id, item);
+        Items.TryAdd(item.Id, item);
 
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
+    }
 
-        public Task<IEnumerable<T>> AllAsync()
-        {
-            var items = Items.Values.ToList();
+    public Task<IEnumerable<T>> AllAsync()
+    {
+        var items = Items.Values.ToArray();
 
-            return Task.FromResult<IEnumerable<T>>(items);
-        }
+        return Task.FromResult<IEnumerable<T>>(items);
+    }
 
-        public Task<T> FindByAsync(string value)
-        {
-            var result = Items.Values.First(item => item.Title == value);
+    public Task<T> FindByAsync(string value)
+    {
+        var result = Items.Values.First(item => item.Title == value);
 
-            return Task.FromResult(result);
-        }
+        return Task.FromResult(result);
+    }
 
-        public Task<T> GetAsync(Guid id)
-        {
-            return Task.FromResult(Items[id]);
+    public Task<T> GetAsync(Guid id)
+    {
+        return Task.FromResult(Items[id]);
+    }
 
-        }
-
-        public Task SaveChangesAsync()
-        {
-            return Task.CompletedTask;
-        }
+    public Task SaveChangesAsync()
+    {
+        return Task.CompletedTask;
     }
 }
